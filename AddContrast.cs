3c423a6f0384b6409e5b98ea122e08b046ee3871
Мария Плетнева <mspletneva_1@edu.hse.ru>
2022-03-09 21:58:53 +0500

    Add project files.

---
+++ AddContrast.cs
@@ -0,0 +1,101 @@
+﻿using PluginInterface;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Configuration;
+
+namespace Contrast
+{
+    [Version(1, 0)]
+    public class ReverseTransform : IPlugin
+    {
+        public string Name
+        {
+            get
+            {
+                return "Повысить контрастность";
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return "Marusya";
+            }
+        }
+
+        public void Transform(Bitmap bitmap)
+        {
+            int contastCoeff = 10;
+            BitmapData sourceData = bitmap.LockBits(new Rectangle(0, 0,
+                                        bitmap.Width, bitmap.Height),
+                                        ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+
+            bitmap.UnlockBits(sourceData);
+
+
+            double contrastLevel = Math.Pow((100.0 + contastCoeff) / 100.0, 2);
+
+
+            double blue;
+            double green;
+            double red;
+
+
+            for (int k = 0; k + 4 < pixelBuffer.Length; k += 4)
+            {
+                blue = ((((pixelBuffer[k] / 255.0) - 0.5) *
+                            contrastLevel) + 0.5) * 255.0;
+
+
+                green = ((((pixelBuffer[k + 1] / 255.0) - 0.5) *
+                            contrastLevel) + 0.5) * 255.0;
+
+
+                red = ((((pixelBuffer[k + 2] / 255.0) - 0.5) *
+                            contrastLevel) + 0.5) * 255.0;
+
+
+                if (blue > 255)
+                { blue = 255; }
+                else if (blue < 0)
+                { blue = 0; }
+
+
+                if (green > 255)
+                { green = 255; }
+                else if (green < 0)
+                { green = 0; }
+
+
+                if (red > 255)
+                { red = 255; }
+                else if (red < 0)
+                { red = 0; }
+
+
+                pixelBuffer[k] = (byte)blue;
+                pixelBuffer[k + 1] = (byte)green;
+                pixelBuffer[k + 2] = (byte)red;
+            }
+
+            BitmapData resultData = bitmap.LockBits(new Rectangle(0, 0,
+                                        bitmap.Width, bitmap.Height),
+                                        ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+
+            Marshal.Copy(pixelBuffer, 0, resultData.Scan0, pixelBuffer.Length);
+            bitmap.UnlockBits(resultData);
+
+        }
+    }
+}
